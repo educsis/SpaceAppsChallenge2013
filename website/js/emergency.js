e1d9b6690f5e2204bceb60d880c27c6ebdb7d9a5
Eduardo Alejandro Avilés Jiménez <eduardo.avilesj@gmail.com>
2013-04-21 14:50:30 -0600

    emergency update

--- website/js/emergency.js
+++ website/js/emergency.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
 
 	//Enable when main page done.
-	//setTimeout(alert,1000);
-
+	//setTimeout(alert,10000);
+	$('#pop').click(changeEmergencyScreen);
 	function alert(){
 		$('#popDiv').bPopup({
             speed: 300,
@@ -11,8 +11,29 @@ $(document).ready(function(){
 			onClose: changeEmergencyScreen
         });
 	}
-	$('#pop').click(changeEmergencyScreen);
-
+	
+	function handlerIn(){
+		$("#quote_water").hide();
+		$("#empty_water").toggle();
+	}
+	function handlerOut(){
+		$("#empty_water").hide();
+		$("#quote_water").toggle();
+	}
+	function solveEmergency(){
+		$('.fire1').bPopup({
+            speed: 450,
+            transition: 'slideDown',
+			
+        });
+	}
+	function solveEmergency2(){
+		$('.fire2').bPopup({
+            speed: 450,
+            transition: 'slideDown',
+			
+        });
+	}
 
 	function changeEmergencyScreen(){
 		window.location.replace("emergency.html");
